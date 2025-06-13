5a5fe9c97521977b278d2089cf8d2be0d773449a
alfaiv <alfaiv@yandex.ru>
2025-06-13 21:12:25 +0300

    add pages

---
+++ src/pages/newsListPage/ui/ListNewsPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import NewsList from 'features/newsList/ui/NewsList.vue';
+import Layout from 'app/Layout.vue';
+</script>
+
+<template>
+  <Layout>
+    <template #header>
+      <p>Header</p>
+    </template>
+
+    <NewsList/>
+
+    <template #footer>
+      <p>Footer</p>
+    </template>
+  </Layout>
+</template>
