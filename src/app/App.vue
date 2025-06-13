8905dab573211b3e3fa1e10701b8069edfbaccb3
alfaiv <alfaiv@yandex.ru>
2025-06-13 18:06:12 +0300

    add template

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
 import NewsList from 'features/newsList/ui/NewsList.vue';
+import Layout from './Layout.vue';
 </script>
 
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>
-        <el-row :gutter="20">
-          <el-col :span="4"></el-col>
-          <el-col :span="16"><NewsList/></el-col>
-          <el-col :span="4"></el-col>
-        </el-row>
-      </el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-  </div>
+  <Layout>
+    <template #header>
+      <p>Header</p>
+    </template>
+
+    <NewsList/>
+
+    <template #footer>
+      <p>Footer</p>
+    </template>
+  </Layout>
 </template>
